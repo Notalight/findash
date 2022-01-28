e8322ffb87c210d55e83a909364bd948168d1692
Christophe Baranger <christophe.baranger@gmail.com>
2022-01-28 15:34:22 +0100

    initial

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import datetime
+from statistics import pvariance
 from sqlalchemy import DateTime, Float, create_engine, text, select
 from sqlalchemy import MetaData
 from sqlalchemy import Table, Column, Integer, String
@@ -11,18 +12,18 @@ from sqlalchemy.orm import sessionmaker
 
 import enum
 
+import pandas as pd
 
-class myClass:
-	def __init__(self,val):
-		self.val=val
-	def getVal(self):
-		return self.val
+from rich import print
+import rich
+from rich.console import Console
+
+
 
 #
 # SQLAlchemy Model
 #
 mapper_registry = registry()
-
 
 @mapper_registry.mapped
 class Account:
@@ -59,3 +60,125 @@ class Stock:
 
     def __repr__(self):
         return "<Stock(%r, %r)>" % (self.name, self.qty)
+
+@mapper_registry.mapped
+class Loan:
+    __tablename__ = "loan"
+    id = Column(Integer, primary_key=True)
+    loan_number = Column(String)
+    name = Column(String)
+    description = Column(String)
+    pv = Column(Float)
+    rate = Column(Float)
+    nper = Column(Float)
+    start_date = Column(DateTime)
+
+    def __repr__(self):
+        return "<Loan(%r, %r)>" % (self.name, self.description)
+
+
+@mapper_registry.mapped
+class Asset:
+    __tablename__ = "asset"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    type = Column(String)
+    acquisition_price = Column(Float)
+    acquisition_date = Column(DateTime)
+    estimation_price = Column(Float)
+
+    def __repr__(self):
+        return "<Asset(%r, %r)>" % (self.name, self.description)
+
+
+#
+# Helper functions
+#
+
+#
+# format currency to display on console
+#
+def format_currency(value, currency):
+    match currency:
+        case 'EUR':
+            if value >= 0:
+                return "{:,.2f} €".format(value)
+            else:
+                return "({:,.2f}) €".format(-value)
+        case 'DOLLAR':
+            if value >= 0:
+                return "{:,.2f} $".format(value)
+            else:
+                return "({:,.2f}) $".format(-value)
+        case _:
+            return "Error conversion"
+
+def display_accounts(accounts):
+    # Format table to show
+    table = rich.table.Table(title="Accounts")
+    table.add_column("RÃ©fÃ©rence", justify="right",
+                     style="cyan", no_wrap=True)
+    table.add_column("Nom", style="magenta")
+    table.add_column("Banque", style="magenta")
+    table.add_column("Type", style="magenta")
+    table.add_column("Valeur", justify="right", style="green")
+    #
+    for account in accounts:
+        table.add_row(account.account_number, account.name, account.bank_name, account.type,
+                      format_currency(account.amount, account.currency))
+    # Display result
+    # TODO why use console
+    console = Console()
+    console.print(table)
+
+
+def display_stocks(stocks):
+    # Format table to show
+    table = rich.table.Table(title="Stocks")
+    table.add_column("Symbol", justify="right", style="cyan", no_wrap=True)
+    table.add_column("Name", style="magenta")
+    table.add_column("Market", style="magenta")
+    table.add_column("Qty", style="magenta")
+    table.add_column("Value", justify="right", style="green")
+    #
+    for stock in stocks:
+        table.add_row(stock.symbol, stock.name, stock.market,
+                      "{:,.2f}".format(stock.qty),
+                      format_currency(stock.purchase_price, stock.currency))
+    # Display result
+    # TODO why use console
+    console = Console()
+    console.print(table)
+
+    def convert_to_dataframe(stocks):
+        symbols = []
+        quantities = []
+        for stock in stocks:
+            symbols.append(stock.symbol)
+            quantities.append(stock.qty)
+        stocks_list = [symbols, quantities]
+        df = pd.DataFrame(stocks_list).transpose()
+        df.columns = ['ticker', 'qty']
+        #df.columns = ['ticker', 'qty','unit price', 'currency', 'total']
+        #print (df)
+        return df
+
+
+
+        
+# class Transaction(Document):
+#     title = StringField(required=True, unique_with = ['bank_account','operation_date','debit','credit'])
+#     bank_account = ReferenceField(Account, required=True)
+#     operation_date = DateTimeField(required=True)
+#     description = StringField()
+#     debit = DecimalField(required=True)
+#     credit = DecimalField(required=True)
+#     devise = EnumField(Devise, default=Devise.EURO)
+
+#     meta = {
+#         'allow_inheritance': True,
+#     #     'indexes': [
+#     #         {'fields': ('title', 'bank_account','operation_date','debit','credit'), 'unique': True}
+#     #     ]
+#     }
--- setup_db.py
+++ setup_db.py
@@ -1,5 +1,8 @@
-from model import Account,Stock
+from model import Account, Stock, Loan, Asset, mapper_registry
 import datetime
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, select
 
 
 #
@@ -19,6 +22,72 @@ bd2 = Account(
     bank_name="Bourse Direct",
     last_update=datetime.datetime(2022, 1, 10)
 )
+
+#
+# Loans
+#
+loan1 = Loan(
+    loan_number="AX05899559",
+    name="0001338324701 ALTIMO FIX",
+    description="Prêt Maison principale",
+    pv=-67190.45,
+    rate=0.0115,
+    nper=66,
+    start_date=datetime.datetime(2021, 2, 5)
+)
+
+loan2 = Loan(
+    loan_number="00002681303",
+    name="CCHQ 65080290572",
+    description="Prêt Appt. Viry Chatillon",
+    pv=-190021,
+    rate=0.0063,
+    nper=120,
+    start_date=datetime.datetime(2021, 7, 22)
+)
+
+loan3 = Loan(
+    loan_number="00002681285",
+    name="CCHQ 65080290262",
+    description="Prêt Appt.  Crépy en Valois",
+    pv=-68325,
+    rate=0.0063,
+    nper=96,
+    start_date=datetime.datetime(2021, 8, 19)
+)
+
+
+#
+# Assets
+#
+asset1 = Asset(
+    name="Appt Crépy",
+    description="Appt Crépy",
+    type="appt",
+    acquisition_price=110000,
+    acquisition_date =datetime.datetime(2017, 7, 1),
+    estimation_price = 130000
+)
+
+asset2 = Asset(
+    name="Maison Igny",
+    description="Maison Igny",
+    type="maison",
+    acquisition_price=740000,
+    acquisition_date =datetime.datetime(2021, 7, 1),
+    estimation_price = 950000
+)
+
+asset3 = Asset(
+    name="Appt Viry",
+    description="Appt Viry",
+    type="appt",
+    acquisition_price=210000,
+    acquisition_date =datetime.datetime(2021, 7, 1),
+    estimation_price = 210000
+)
+
+
 #
 # Stocks
 #
@@ -70,16 +139,55 @@ stock4 = Stock(
     currency_fees="EUR",
 )
 
+#
+#
+#
 
-def fill_bd(session):
-    print("Filling Db...")
+
+def populate(session):
 
     session.add_all(
         [
             bd1, bd2,
-            stock1, stock2, stock3, stock4
+            stock1, stock2, stock3, stock4,
+            loan1, loan2, loan3,
+            asset1, asset2, asset3
         ]
     )
-    print(bd1.bank_name)
-    print(bd1.id)
+
     session.commit()
+
+    select_statement = select(Account)
+    result = session.execute(select_statement)
+    for account_obj in result.scalars():
+        print("Account : ", account_obj)
+
+    select_statement = select(Stock)
+    result = session.execute(select_statement)
+    for stock_obj in result.scalars():
+        print("Stock : ", stock_obj)
+
+    select_statement = select(Loan)
+    result = session.execute(select_statement)
+    for loan_obj in result.scalars():
+        print("Loan : ", loan_obj)
+
+    select_statement = select(Asset)
+    result = session.execute(select_statement)
+    for asset_obj in result.scalars():
+        print("Asset : ", asset_obj)
+
+
+#
+#
+#
+
+def create_db_and_tables(db_file_name):
+    engine = create_engine(
+        f"sqlite:///{db_file_name}", echo=False, future=True)
+    with engine.begin() as connection:
+        mapper_registry.metadata.create_all(connection)
+
+    Session = sessionmaker(bind=engine, future=True)
+    session = Session()
+    return session
