72f9642d474bbdd879af77bda4f37dadc41b7387
Christophe Baranger <christophe.baranger@gmail.com>
2022-02-07 23:37:28 +0100

    initial

--- app.py
+++ app.py
@@ -1,53 +1,62 @@
 import datetime
-
-from sqlalchemy import DateTime, Float, create_engine, text, select
-from sqlalchemy import MetaData
-from sqlalchemy import Table, Column, Integer, String
-from sqlalchemy.orm import registry
-from sqlalchemy.orm import relationship
-from sqlalchemy import text
-from sqlalchemy import Column, Integer, String
-
+#
 import pandas as pd
 from pandas_datareader import test
-import warnings
 import matplotlib.pyplot as plt
 import numpy as np
-
-
-
-
+#
+import yfinance as yf
+import quandl
+#
 import enum
 
-from setup_db import *
-
-
-
+from model import *
 
 #
-# Setup SQLAchemy
+# load portfolio
 #
-#mapper_registry = registry()
-# engine = create_engine("sqlite:///:memory:", echo=True, future=True)
-# with engine.begin() as connection:
-#     mapper_registry.metadata.create_all(connection)
-
-# Session = sessionmaker(bind=engine, future=True)
-# session = Session()
-
-session = create_db_and_tables(":memory:")
-print("Populate...")
-populate(session)
-
+portfolio = load_portfolio('portfolio.json')
+portfolio.show_accounts()
+# 
+# Quandl : keys
 #
+QUANDL_KEY = "zSQgVZXfyBf_ek_efub5"
+quandl.ApiConfig.api_key = QUANDL_KEY
+#
+df_yahoo = yf.download(['CRM'   , 'DDD', 'PRLB'],
+                       start='2018-01-01',
+                       end='2022-1-28',
+                       # auto_adjust=True,
+                       progress=False)
 
+df = df_yahoo.loc[:, ['Adj Close']]
+df.rename(columns={'Adj Close': 'adj_close'}, inplace=True)
 
+# Download inflation data from Quandl
+dates = pd.date_range(
+    start='2018-01-01',
+    end='2022-01-28'
+)
 
-
-
-
-
-
+# Download inflation data from Quandl
+df_cpi = quandl.get(dataset='RATEINF/CPI_FRA', 
+                    start_date='2018-01-01', 
+                    end_date='2022-1-28')
+df_cpi.rename(columns={'Value':'cpi'}, inplace=True)
+# Create a DataFrame with all possible dates and left join the prices on it
+df_all_dates = pd.DataFrame(index=pd.date_range(start='2018-01-01',
+                                                end='2022-01-28'))
+df = df_all_dates.join(df[['adj_close']], how='left') \
+                 .fillna(method='ffill') \
+                 .asfreq('M')
+# Merge inflation data to prices:              
+df_merged = df.join(df_cpi, how='left')
+# Calculate simple returns and inflation rate
+df_merged['simple_rtn'] = df_merged.adj_close.pct_change()
+df_merged['inflation_rate'] = df_merged.cpi.pct_change()
+# Adjust returns for inflation
+df_merged['real_rtn'] = (df_merged.simple_rtn + 1) / (df_merged.inflation_rate + 1) - 1
+df_merged.head()
 
 # result = pd.read_sql(text('SELECT name FROM students'), conn)
 # print(result)
--- model.py
+++ model.py
@@ -1,184 +1,111 @@
-import datetime
-from statistics import pvariance
-from sqlalchemy import DateTime, Float, create_engine, text, select
-from sqlalchemy import MetaData
-from sqlalchemy import Table, Column, Integer, String
-from sqlalchemy.orm import registry
-from sqlalchemy.orm import relationship
-from sqlalchemy.orm import Session
-from sqlalchemy import text
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+from typing import List, Optional
+from pydantic import BaseModel
+from pydantic import ValidationError
 
-import enum
 
-import pandas as pd
-
-from rich import print
 import rich
+#from rich import print
 from rich.console import Console
 
+console = Console()
 
 
-#
-# SQLAlchemy Model
-#
-mapper_registry = registry()
-
-@mapper_registry.mapped
-class Account:
-    __tablename__ = "account"
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    account_number = Column(String)
-    type = Column(String)
-    bank_name = Column(String)
-    last_update = Column(DateTime)
-
-    def __repr__(self):
-        return "<Account(%r, %r)>" % (self.name, self.bank_name)
+class Asset(BaseModel):
+    name: str
+    description: str
+    type: str
+    acquisition_price: float
+    acquisition_date: str
+    estimation_price: float
 
 
-@mapper_registry.mapped
-class Stock:
-    __tablename__ = "stock"
-
-    id = Column(Integer, primary_key=True)
-    order_number = Column(String)
-    name = Column(String)
-    symbol = Column(String)
-    market = Column(String)
-    qty = Column(Float)
-    purchase_date = Column(DateTime)
-    unit_purchase_price = Column(String)
-    purchase_change_rate = Column(String)
-    #currency = Column(enum.Enum('EUR', 'DOLLAR'))
-    currency = Column(String)
-    fees = Column(String)
-    currency_fees = Column(String)
-
-    def __repr__(self):
-        return "<Stock(%r, %r)>" % (self.name, self.qty)
-
-@mapper_registry.mapped
-class Loan:
-    __tablename__ = "loan"
-    id = Column(Integer, primary_key=True)
-    loan_number = Column(String)
-    name = Column(String)
-    description = Column(String)
-    pv = Column(Float)
-    rate = Column(Float)
-    nper = Column(Float)
-    start_date = Column(DateTime)
-
-    def __repr__(self):
-        return "<Loan(%r, %r)>" % (self.name, self.description)
+class Loan(BaseModel):
+    loan_number: str
+    name: str
+    description: str
+    pv: float
+    rate: float
+    nper: int
+    start_date: str
 
 
-@mapper_registry.mapped
-class Asset:
-    __tablename__ = "asset"
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    description = Column(String)
-    type = Column(String)
-    acquisition_price = Column(Float)
-    acquisition_date = Column(DateTime)
-    estimation_price = Column(Float)
-
-    def __repr__(self):
-        return "<Asset(%r, %r)>" % (self.name, self.description)
-
-
-#
-# Helper functions
-#
-
-#
-# format currency to display on console
-#
-def format_currency(value, currency):
-    match currency:
-        case 'EUR':
-            if value >= 0:
-                return "{:,.2f} €".format(value)
-            else:
-                return "({:,.2f}) €".format(-value)
-        case 'DOLLAR':
-            if value >= 0:
-                return "{:,.2f} $".format(value)
-            else:
-                return "({:,.2f}) $".format(-value)
-        case _:
-            return "Error conversion"
-
-def display_accounts(accounts):
-    # Format table to show
-    table = rich.table.Table(title="Accounts")
-    table.add_column("RÃ©fÃ©rence", justify="right",
-                     style="cyan", no_wrap=True)
-    table.add_column("Nom", style="magenta")
-    table.add_column("Banque", style="magenta")
-    table.add_column("Type", style="magenta")
-    table.add_column("Valeur", justify="right", style="green")
-    #
-    for account in accounts:
-        table.add_row(account.account_number, account.name, account.bank_name, account.type,
-                      format_currency(account.amount, account.currency))
-    # Display result
-    # TODO why use console
-    console = Console()
-    console.print(table)
+class Stock(BaseModel):
+    order_number: str
+    name: str
+    symbol: str
+    market: str
+    qty: float
+    purchase_date: str
+    unit_purchase_price: float
+    purchase_change_rate: Optional[float] = 1.0
+    currency: str
+    fees: float
+    currency_fees: str
 
 
-def display_stocks(stocks):
-    # Format table to show
-    table = rich.table.Table(title="Stocks")
-    table.add_column("Symbol", justify="right", style="cyan", no_wrap=True)
-    table.add_column("Name", style="magenta")
-    table.add_column("Market", style="magenta")
-    table.add_column("Qty", style="magenta")
-    table.add_column("Value", justify="right", style="green")
-    #
-    for stock in stocks:
-        table.add_row(stock.symbol, stock.name, stock.market,
-                      "{:,.2f}".format(stock.qty),
-                      format_currency(stock.purchase_price, stock.currency))
-    # Display result
-    # TODO why use console
-    console = Console()
-    console.print(table)
-
-    def convert_to_dataframe(stocks):
-        symbols = []
-        quantities = []
-        for stock in stocks:
-            symbols.append(stock.symbol)
-            quantities.append(stock.qty)
-        stocks_list = [symbols, quantities]
-        df = pd.DataFrame(stocks_list).transpose()
-        df.columns = ['ticker', 'qty']
-        #df.columns = ['ticker', 'qty','unit price', 'currency', 'total']
-        #print (df)
-        return df
+class Account(BaseModel):
+    id: str
+    name: str
+    account_number: str
+    type: str
+    bank_name: str
+    loans: Optional[list[Loan]] = []
+    stocks: Optional[list[Stock]] = []
 
 
+class Portfolio(BaseModel):
+    id: str
+    name: str
+    accounts: list[Account]
+    assets: Optional[list[Asset]] = None
 
+    def get_loans(self):
+        loans = []
+        for account in self.accounts:
+            for loan in account.loans:
+                loans.append(loan)
+        return loans
+
+    def show_accounts(self):
+        # Format table to show
+        table = rich.table.Table(title="Accounts")
+        table.add_column("", justify="right",
+                         style="blue", no_wrap=True)
+        table.add_column("Name", justify="left",
+                         style="cyan", no_wrap=True)
+        table.add_column("Bank Name", style="magenta")
+        table.add_column("Type", style="green")
+        table.add_column("Account Number", style="magenta", justify="right")
+        count = 1
+        for account in self.accounts:
+            table.add_row(str(count), account.name,
+                          account.bank_name, account.type, account.account_number)
+            count += 1
+        console.print(table)
         
-# class Transaction(Document):
-#     title = StringField(required=True, unique_with = ['bank_account','operation_date','debit','credit'])
-#     bank_account = ReferenceField(Account, required=True)
-#     operation_date = DateTimeField(required=True)
-#     description = StringField()
-#     debit = DecimalField(required=True)
-#     credit = DecimalField(required=True)
-#     devise = EnumField(Devise, default=Devise.EURO)
+    def get_loans(self):
+        loans = []
+        for account in self.accounts:
+            for loan in account.loans:
+                loans.append(loan)
+        return loans        
 
-#     meta = {
-#         'allow_inheritance': True,
-#     #     'indexes': [
-#     #         {'fields': ('title', 'bank_account','operation_date','debit','credit'), 'unique': True}
-#     #     ]
-#     }
+
+def load_portfolio(path: str):
+    portfolio_json = open(path, 'r').read()
+    portfolio = Portfolio.parse_raw(portfolio_json)
+    return portfolio
+
+def save_portfolio(path: str):
+    pass
+
+portfolio = load_portfolio('portfolio.json')
+
+#portfolio_json = open('portfolio.json', 'r').read()
+# jsonpickle.set_preferred_backend('simplejson')
+# jsonpickle.load_backend(simplejson)
+
+# portfolio.show_accounts()
+
+# portfolio.get_loans()
